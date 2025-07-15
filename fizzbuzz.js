8070bae7b8a384899a9831393cd4a096816b9f71
Emilie VINCENT-SULLY <emivsy@gmail.com>
2025-07-15 13:16:48 +0200

    exercice completed

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+const fizzbuzz = (number) => {
+  
+  if (number % 3 == 0) {
+    return "Fizz"
+  }
+  if (number === 5) {
+    return "Buzz"
+  }
+  return number
+}
+
+module.exports = { fizzbuzz }
+
